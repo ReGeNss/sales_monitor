6d79d83ab2e44b08ae698cca5da48c631f6e50fe
Regens <regens.global@gmail.com>
2026-01-18 13:30:03 +0200

    fix product differentiator

--- scraper_app/shared/product/utils/product_differentiator.go
+++ scraper_app/shared/product/utils/product_differentiator.go
@@ -7,12 +7,15 @@ import (
 )
 
 func ProductDifferentiator(fingerprint1 string, fingerprint2 string, productDifferentiationEntity *entity.ProductDifferentiationEntity) bool {
+	if productDifferentiationEntity == nil {
+		return true
+	}
 	for _, element := range productDifferentiationEntity.Elements {
-		var  match1 = false
-		var  match2 = false
+		var match1 = false
+		var match2 = false
 		for _, e := range element {
 			re := regexp.MustCompile(fmt.Sprintf(`\s%s\s`, e))
-			
+
 			if re.MatchString(fingerprint1) {
 				match1 = true
 				continue
@@ -21,7 +24,7 @@ func ProductDifferentiator(fingerprint1 string, fingerprint2 string, productDiff
 
 		for _, e := range element {
 			re := regexp.MustCompile(fmt.Sprintf(`\s%s\s`, e))
-			
+
 			if re.MatchString(fingerprint2) {
 				match2 = true
 				continue
